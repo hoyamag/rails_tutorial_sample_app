e73c06bab8a7235af771e979b49211569737ebd6
hoyamag <2036oshmkufafg36@gmail.com>
2018-07-19 12:38:47 +0000

    The end of 4.2.5

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
-module ApplicationHelper
+module ApplicationHelper # declare a module name which contents of this file belong to.
+  # ページごとの完全なタイトルを返します。
+  def full_title(page_title = '')  # define method
+    base_title = "Ruby on Rails Tutorial Sample App"  # substitute to a variable
+    if page_title.empty?  # test logical value
+      base_title  # implicit return value
+    else
+      page_title + " | " + base_title  # concatenate strings
+    end
+  end 
 end
